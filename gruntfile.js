4f60e662046cfb23f6a6d8e84dbb04db243b1b5d
uncleF <uncle.funkay@gmail.com>
2016-02-24 18:56:15 +0300

    Project Update

--- gruntfile.js
+++ gruntfile.js
@@ -502,7 +502,7 @@ module.exports = function(grunt) {
       },
       templates: {
         cwd: project.res.templates.dir,
-        src: ['*.html'],
+        src: ['*.html', '!* copy.html', '!* - Copy.html'],
         dest: project.dir,
         expand: true
       }
@@ -731,7 +731,7 @@ module.exports = function(grunt) {
         spawn: false
       },
       html: {
-        files: [project.res.templates.dir + '**/*.html', project.res.templates.dir + '!**/* copy.html', project.res.templates.dir + '!**/* - Copy.html'],
+        files: [project.res.templates.dir + '**/*.html'],
         tasks: ['processhtml']
       },
       images: {
